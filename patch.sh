3dbfec1323b5b5f37e2b991f539cafc9b2e0c04b
Fehér Roland Ádám <feherneoh@gmail.com>
2014-04-18 20:07:11 +0200

    Add support for .patch files

--- patch.sh
+++ patch.sh
@@ -3,17 +3,31 @@ if [ -d sourcefix ]
 then
 	echo Patching source tree...
 	cd sourcefix
-	FILELIST=$(find .)
+	echo Replacing files...
+	FILELIST=$(find .|grep --invert-match .patch)
 	for FILETOPATCH in $FILELIST;
 	do
 		if [ -e ../$FILETOPATCH ]
 		then
-			echo Patching $FILETOPATCH
 			cp $FILETOPATCH ../$FILETOPATCH
 		else
 			echo $FILETOPATCH does not exist
 		fi
 	done
+	echo Done
+	echo Applying .patch files
+	FILELIST=$(find .|grep .patch)
+	for FILETOPATCH in $FILELIST;
+	do
+		ORIGFILE=$(echo $FILETOPATCH|cut -d'.' --complement -f3)
+		if [ -e ../$ORIGFILE ]
+		then
+			patch ../$ORIGFILE < $FILETOPATCH
+		else
+			echo $FILETOPATCH does not exist
+		fi
+	done
+	echo Done
 else
 	echo No patches were downloaded
 fi
